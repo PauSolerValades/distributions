0823cd6eea62605b5e4e682f972d0c9bffcc6367
Pau Soler Valadés <pau.soler.valades@gmail.com>
2026-08-09 18:06:15 +0200

    added a release, removed run and install from original build.zig

--- build.zig
+++ build.zig
@@ -9,18 +9,6 @@ pub fn build(b: *std.Build) void {
         .target = target,
     });
 
-    const exe = b.addExecutable(.{
-        .name = "distributions",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/root.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "distributions", .module = mod },
-            },
-        }),
-    });
-
     // const dtest_mod = b.addModule("dtest", .{
     //     .root_source_file = b.path("test/kolmogorov-smirnov.zig"),
     //     .target = target,
@@ -72,21 +60,6 @@ pub fn build(b: *std.Build) void {
         examples_step.dependOn(&example_cmd.step);
     }
 
-    b.installArtifact(exe);
-
-    const run_step = b.step("run", "Run the app");
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
     // Creates an executable that will run `test` blocks from the provided module.
     // Here `mod` needs to define a target, which is why earlier we made sure to
     // set the releative field.
@@ -96,32 +69,11 @@ pub fn build(b: *std.Build) void {
 
     const run_mod_tests = b.addRunArtifact(mod_tests);
 
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
 
-    // Release step: runs tests + gof, then compiles everything in ReleaseSafe
-    const release_step = b.step("release", "Run tests and gof, then build in ReleaseSafe");
+    // Release step: runs tests + gof.
+    const release_step = b.step("release", "Run tests and gof");
     release_step.dependOn(test_step);
     release_step.dependOn(gof_step);
-
-    const release_exe = b.addExecutable(.{
-        .name = "distributions",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = .ReleaseSafe,
-            .imports = &.{
-                .{ .name = "distributions", .module = mod },
-            },
-        }),
-    });
-    const install_release_exe = b.addInstallArtifact(release_exe, .{});
-    release_step.dependOn(&install_release_exe.step);
 }
